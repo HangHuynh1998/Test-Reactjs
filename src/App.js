7abe0622f2a85a74fb182e4a2a62664b9239a802
HangHuynh1998 <huynhhang.24hdev@gmail.com>
2020-08-27 16:07:00 +0700

    Init UI

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { Container, Button } from "react-bootstrap";
+import ContactModal from "./component/ContactModal";
+import classes from "./style/Modal.module.css";
+import Contact from "./component/Contact";
+import { useState } from "react";
+import Contacts from "./component/Contacts";
+function App() {
+  const [showContacts, setShowContacts] = useState(true)
+  const [showContactDetail, setShowContactDetail] = useState(false)
+  
+  const onSelectedActiveContact = () => {
+    console.log("onSelectedActiveContact");
+    setShowContacts(false)
+    setShowContactDetail(true)
+  }
+
+  const onCloseDetail = () => {
+    console.log("onCloseDetail");
+    setShowContactDetail(false)
+    setShowContacts(true)
+  }
+  return (
+    <BrowserRouter>
+      <Container className="text-center">
+        <Link to="/all-contacts">
+          <Button variant="primary" className={classes.ButtonA}>
+            All Contacts
+          </Button>
+        </Link>
+        <Link to="/us-contacts">
+          <Button className={classes.ButtonB}>US Contacts</Button>
+        </Link>
+        <Contact showContactDetail={showContactDetail} onCloseContact={onCloseDetail} /> 
+        <Switch>
+          <Route
+            exact
+            path="/all-contacts"
+            
+          >
+            <Contacts showContacts={showContacts} selectActiveContact={onSelectedActiveContact} />
+          </Route>
+          <Route
+            exact
+            path="/us-contacts"
+            
+          >
+            <Contacts showContacts={showContacts} selectActiveContact={onSelectedActiveContact} />
+          </Route>
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  );
+}
+
+export default App;
